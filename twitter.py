d81fec4a16ff1bc26875c4fef0a11e0e61604abc
kresthill <155745208+kresthill@users.noreply.github.com>
2025-11-25 10:58:48 +0100

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -11,7 +11,7 @@ API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
-BEARER_TOKEN = os.getenv("BEARER_TOKEN")  # You'll need to add this to your .env file
+BEARER_TOKEN = os.getenv("BEARER_TOKEN") 
 
 # Authenticate with API v2 using Bearer Token
 client = tweepy.Client(
